0e7333b3ae43a83c7df4aef894d33091c01b7178
soniafaiza <soniabenchebana@gmail.com>
2021-10-31 11:30:58 -0400

    Added UserService, UserRepository and Controller methods

--- Project2_ReadMe/src/main/java/com/ReadMe/controller/FrontController.java
+++ Project2_ReadMe/src/main/java/com/ReadMe/controller/FrontController.java
@@ -1,18 +1,31 @@
 package com.ReadMe.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ReadMe.model.User;
 import com.ReadMe.service.BookService;
 import com.ReadMe.service.UserService;
+
 
 import lombok.NoArgsConstructor;
 
 @RestController
-@RequestMapping(value="/idk")
-@CrossOrigin(origins="*")
+@RequestMapping(value="/users")
+@CrossOrigin(origins="*")// we are allowing any domaine to request the server 
 @NoArgsConstructor
 public class FrontController {
 	private UserService uServ;
@@ -24,4 +37,50 @@ public class FrontController {
 		this.uServ = uServ;
 		this.bServ = bServ;
 	}
+	
+	@GetMapping("/initial")
+    public ResponseEntity<List<User>>  insertInitialValues(){
+    	List<User> uList = new ArrayList<User>(Arrays.asList(new User("user1","password1","sonia", "bench","sonia@gmail.com","customer")));
+    	for (User user:uList) {
+    		uServ.insertUser(user);
+    	}
+        return new ResponseEntity<List<User>>(uServ.getAllUsers(), HttpStatus.CREATED);
+    }
+
+	  @GetMapping()
+      public ResponseEntity<List<User>>getAllUsers(){
+   	   return new ResponseEntity<List<User>>(uServ.getAllUsers(), HttpStatus.OK);
+	  }
+	
+	  @GetMapping("/{username}")
+      public ResponseEntity<User> getUserByUsername(@PathVariable("username") String username){
+   	   User user = uServ.getUserByUsername(username);
+   	   if(user==null) {
+   		   return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+   	   }
+   	    return new ResponseEntity<>(null, HttpStatus.OK);
+   	   }
+      
+      @DeleteMapping("/{foodname}")
+      public ResponseEntity<String> deleteUser(@PathVariable("username") String username){
+   	  User user = uServ.getUserByUsername(username);
+   	   if(user==null) {
+   		   return new ResponseEntity<String>("No user of that name was found", HttpStatus.NOT_FOUND);
+      }
+      uServ.deleteUser(user);
+      return new ResponseEntity<String>("user was deleted", HttpStatus.NOT_FOUND);
+      
+      }
+      
+      @PostMapping()
+      public ResponseEntity<Object> insertUser(@RequestBody User user){
+   	   System.out.println(user);
+   	   if(uServ.getUserByUsername(user.getUsername()) != null) {
+   		   return new ResponseEntity<>("user of that name already exists",HttpStatus.FORBIDDEN);
+   	   }
+   	   uServ.insertUser(user);
+   	   return new ResponseEntity<>(uServ.getUserByUsername(user.getUsername()), HttpStatus.CREATED);
+      }
+	  
+
 }
--- Project2_ReadMe/src/main/java/com/ReadMe/repository/UserRepository.java
+++ Project2_ReadMe/src/main/java/com/ReadMe/repository/UserRepository.java
@@ -1,11 +1,27 @@
 package com.ReadMe.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.ReadMe.model.User;
 
+
+
 @Repository
 public interface UserRepository extends JpaRepository<User, String>{
-
+	
+	public List<User> findAll();
+	public User findByUserName( String username);
+	public User findByUserPassword( String password);
+	
+	public User findUserByUsernameAndPassword(String userame, String password);
+	
+	
+	public User findByUserFirstname( String firstname);
+	public User findByUserLastname( String lastname);
+	public User findByUserEmail( String email);
+	public User findByUserRolid(int roleid);
+       
 }
