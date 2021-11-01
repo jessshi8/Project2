2d06ddcded036c1d4549d0fd3a6bca3b1197d4f3
Jessica Shi <jessicaaa.shi@gmail.com>
2021-11-01 12:18:46 -0500

    added search categories

--- Project2Angular/src/app/search/search.component.ts
+++ Project2Angular/src/app/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,13 +8,20 @@ import { Router } from '@angular/router';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  searchGroup = new FormGroup({
+    author: new FormControl(''),
+    title: new FormControl(''),
+    isbn: new FormControl(''),
+    publisher: new FormControl(''),
+    genre: new FormControl('')
+  });
 
   constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
-  searchBooks(keyword : string) {
+  searchBooks(search: FormGroup, keyword : string) {
     console.log('Searched for', keyword);
     this.router.navigateByUrl('/search/'+keyword);
   }
