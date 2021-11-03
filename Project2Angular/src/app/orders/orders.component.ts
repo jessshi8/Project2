c5567d57dd77f47e0fe37cf3ad837ad97f55e81c
soniafaiza <soniabenchebana@gmail.com>
2021-11-02 22:05:10 -0400

    orders component in progress

--- Project2Angular/src/app/orders/orders.component.ts
+++ Project2Angular/src/app/orders/orders.component.ts
@@ -1,4 +1,7 @@
+import { Book } from './../book';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { OrdersService } from './orders.service';
 
 @Component({
   selector: 'app-orders',
@@ -6,10 +9,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./orders.component.css']
 })
 export class OrdersComponent implements OnInit {
+  orderList: Book[] = [];
 
-  constructor() { }
+  orderGroup = new FormGroup({
+    bookCover: new FormControl(''),
+    bookTitle: new FormControl(''),
+    bookAuthor: new FormControl(''),
+    bookPublisher: new FormControl(''),
+    bookPublished: new FormControl(''),
+    bookGenre: new FormControl(''),
+    bookPrice: new FormControl(''),
+    bookSummary: new FormControl('')
+  });
+
+  
+  constructor(private bookServ:OrdersService) { }
 
   ngOnInit(): void {
+    this.bookServ.getAllBooks().subscribe(
+      response =>{
+        console.log(response);
+        this.orderList=response;
+      }
+    )
+    }
+    public submitBook(book: FormGroup){
+      console.log(book);
+      let stringBook = JSON.stringify(book.value);
+      this.bookServ.insertBook(stringBook).subscribe(
+        response => {
+          console.log(response);
+          this.orderList.push(response);
+        },
+        error =>{
+          console.warn("there was an error ", error);
+        }
+      )
+    }
+  
   }
+  
 
-}
+  
+
